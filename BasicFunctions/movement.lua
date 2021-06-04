0d555e87a513acba661670931c89e4323a685026
AverageOtter <amazingasriel2001@gmail.com>
2021-06-04 01:03:19 -0700

    Update to movement.lua

---
+++ BasicFunctions/movement.lua
@@ -0,0 +1,46 @@
+--[[Author: Average_Otter]]--
+--[[Purpose: To Create Basic Movement Function]]--
+-- Forward
+function moveFoward(times)
+    if times then
+        for i = 1, times do
+            while not turtle.forward() do
+            --Could be another turtle should turtle.inspect
+            --[[
+                local has_block, data = turtle.inspect()
+                if has_block then
+                    if data["name"] then
+                        
+                    end
+                
+                end
+            ]]--
+                turtle.dig()
+            end
+        end
+    else
+        turtle.forward()
+    end
+end
+
+-- Turn Left
+function turnLeft(times)
+    if times then
+        for i = 1, times do
+            turtle.turnLeft()
+        end
+    else
+        turtle.turnLeft()
+    end
+end
+
+-- Turn Right
+function turnRight(times)
+    if times then
+        for i = 1, times do
+            turtle.turnRight()
+        end
+    else
+        turtle.turnRight()
+    end
+end
