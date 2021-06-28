f1acb5609155ce7b23784bb5f3274998356ccdea
AverageOtter <amazingasriel2001@gmail.com>
2021-06-27 18:49:36 -0700

    Fixed added some improvements

--- PeriphalsPlusOne/Chatbox/ChattyPC.lua
+++ PeriphalsPlusOne/Chatbox/ChattyPC.lua
@@ -2,10 +2,14 @@
 
 function main()
     chtbx = peripheral.wrap("right")
+    print("Running Chatbox Script : Press Ctrl + T (hold) to terminate")
     while true do
         local event, playername, message = os.pullEvent("chat")
         if playername == "Average_Otter" then
+            sleep(5)
             chtbx.tell("Average_Otter", "Shush RudderButt")
+        elseif message == "hello" then
+            chtbx.say("Hello There!")
         end
     end
 end
