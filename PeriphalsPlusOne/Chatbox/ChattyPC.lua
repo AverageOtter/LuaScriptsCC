132ccc9e3fc2c4afc5e74c3b41a9bcded75e8990
AverageOtter <amazingasriel2001@gmail.com>
2021-06-27 17:28:11 -0700

    Variable Change

--- PeriphalsPlusOne/Chatbox/ChattyPC.lua
+++ PeriphalsPlusOne/Chatbox/ChattyPC.lua
@@ -1,13 +1,13 @@
 ---@diagnostic disable: undefined-global, undefined-field
 
 function main()
-    local chatbox = periphial.wrap("right")
+    local chattybxy = periphial.wrap("right")
     while true do
         local eventtype = os.pullEvent()
         if eventtype then
             local event, playername, message = os.pullEvent("chat")
             if playername == "Average_Otter" then
-                chatbox.tell("Average_Otter", "Shush RudderButt")
+                chattybxy.tell("Average_Otter", "Shush RudderButt")
             end
         end
     end
