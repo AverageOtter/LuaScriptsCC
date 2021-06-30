102ccdebd1ff45c3c9483b3630a46d2e8a81d30c
AverageOtter <amazingasriel2001@gmail.com>
2021-06-30 01:51:02 -0700

    update to advperiphals

--- AdvancedPeriphials/ChatBox/ChattyPC.lua
+++ AdvancedPeriphials/ChatBox/ChattyPC.lua
@@ -4,18 +4,18 @@ function main()
     print("Running Chatbox Script : Press Ctrl + T (hold) to terminate")
     while true do
         local event, playername, message = os.pullEvent("chat")
+        --Messages
+        if message == "hello" then
+            chtbx.sendMessage("Hello There " + playername + "!")
+        elseif message == "uwu" or message == "owo" then
+            chtbx.sendMessage("This is a '" + message + "' free zone! 300$ fine!")
         --Players
-        if playername == "Average_Otter" then
+        elseif playername == "Average_Otter" then
             sleep(4)
             chtbx.sendMessageToPlayer("Shush RudderButt.", playername)
         elseif playername == "KazyHachi" then
             sleep(4)
             chtbx.sendMessageToPlayer("You're Cute owoowowowowo!", playername)
-        --Messages
-        elseif message == "hello" then
-            chtbx.sendMessage("Hello There " + playername + "!")
-        elseif message == "uwu" or message == "owo" then
-            chtbx.sendMessage("This is a '" + message + "' free zone! 300$ fine!")
         end
 
     end
