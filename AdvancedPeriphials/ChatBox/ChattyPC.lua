969c8baed19f5071f34d4182f7b303931c9c1c74
AverageOtter <amazingasriel2001@gmail.com>
2021-07-08 00:58:01 -0700

    Added a Log file

--- AdvancedPeriphials/ChatBox/ChattyPC.lua
+++ AdvancedPeriphials/ChatBox/ChattyPC.lua
@@ -2,37 +2,32 @@
 local greeting = {"hello", "hey", "o/", "ello", "howdy", "hello there"}
 local fines = {"uwu", "owo", "awoo", "o3o", "ono"}
 
-function findLargestTable()
-    local max = -1
-    if #greeting > #fines then 
-        max = #greeting
-    else
-        max = #fines
-    end
-    return max
-end
 
 function main()
     chtbx = peripheral.find("chatBox") --Wraps connected chatbox
     if chtbx == nil then error("Missing ChatBox") end --Errors if chatbox isnt connected.
+    file = io.open("ChatboxOutput.txt", "a+" )
+    io.output(file)
     term.clear()
     term.setCursorPos(1,1)
-    print("Running Chatbox: Hold Ctrl + T to terminate\n") --Prints to let user know what script is Running
-    print("--------------------------------\n")
+    print("Running Chatbox: Hold Ctrl + T to terminate") --Prints to let user know what script is Running
+    print("----------------------------------")
     while true do
         local event, playername, message = os.pullEvent("chat") --Grabs data from chat event
         local str = string.lower(message)
         for setCount = #greeting, 1, -1 do
             if string.find(str, greeting[setCount]) ~= nil then
                 chtbx.sendMessage("Hello There " .. playername .. "!")
-                print("- Hello There " .. playername .. "!\n")
+                io.write(os.date() .. " - Hello There " .. playername .. "! : " .. playername .. "\n")
+                sleep(2)
             end
         end
         for setCount = #fines, 1, -1 do
             if string.find(str, fines[setCount]) ~= nil then
-                local fineAmount = math.random(500)
+                local fineAmount = math.random(100, 500)
                 chtbx.sendMessage("This is a \'" .. fines[setCount] .. "\' free zone! ".. fineAmount.."$ fine!")
-                print("- This is a \'" .. fines[setCount] .. "\' free zone! ".. fineAmount.."$ fine!\n")
+                io.write(os.date() .. " - This is a \'" .. fines[setCount] .. "\' free zone! ".. fineAmount.."$ fine! : " .. playername .."\n")
+                sleep(2)
             end
         end
     end
