225726d3766d673926add12aa4949fa766bbf6ed
AverageOtter <amazingasriel2001@gmail.com>
2021-07-08 00:14:38 -0700

    Removed IO stuff

--- AdvancedPeriphials/ChatBox/ChattyPC.lua
+++ AdvancedPeriphials/ChatBox/ChattyPC.lua
@@ -15,23 +15,25 @@ end
 function main()
     chtbx = peripheral.find("chatBox") --Wraps connected chatbox
     if chtbx == nil then error("Missing ChatBox") end --Errors if chatbox isnt connected.
-    io.flush()
-    io.write("Running Chatbox Script : Press Ctrl + T (hold) to terminate") --Prints to let user know what script is Running
-    io.write("----------------------------------------------------")
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Running Chatbox: Hold Ctrl + T to terminate\n") --Prints to let user know what script is Running
+    print("--------------------------------\n")
     local SetCount = 0
     local largestSet = findLargestTable()
     if largestSet == -1 then error("Invalid table length") end
     while true do
         local event, playername, message = os.pullEvent("chat") --Grabs data from chat event
+        print(playername .. ", " .. message .. "\n")
         for setCount = largestSet, 1, -1 do
             local str = string.lower(message)
             if string.find(str, greeting[setCount]) ~= nil then
                 chtbx.sendMessage("Hello There " .. playername .. "!")
-                io.write("- Hello There " .. playername .. "!")
+                --print("- Hello There " .. playername .. "!\n")
             elseif string.find(str, fines[setCount]) ~= nil then
                 local fineAmount = math.random(500)
                 chtbx.sendMessage("This is a \'" .. fines[setCount] .. "\' free zone! ".. fineAmount.."$ fine!")
-                io.write("- This is a \'" .. fines[setCount] .. "\' free zone! ".. fineAmount.."$ fine!")
+                --print("- This is a \'" .. fines[setCount] .. "\' free zone! ".. fineAmount.."$ fine!\n")
             end
         end
     end
